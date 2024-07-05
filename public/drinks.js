7a7c89ee9cd55936f7dc0a1ede8ae165f052eb91
unknown <anna06491@gmail.com>
2024-07-05 19:29:06 +0200

    drinks list

---
+++ public/drinks.js
@@ -0,0 +1,352 @@
+export const drinks =
+    [
+        {
+          "id": 0,
+          "name": "Espresso",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/espresso.png"
+        },
+        {
+          "id": 1,
+          "name": "Doppio",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/doppio.png"
+        },
+        {
+          "id": 2,
+          "name": "Lungo",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#b0dee1",
+              "label": "voda"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/lungo.png"
+        },
+        {
+          "id": 3,
+          "name": "Ristretto",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/ristretto.png"
+        },
+        {
+          "id": 4,
+          "name": "Macchiato",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "mléčná pěna"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/macchiato.png"
+        },
+        {
+          "id": 5,
+          "name": "Corretto",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#fa872f",
+              "label": "likér"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/corretto.png"
+        },
+        {
+          "id": 6,
+          "name": "Con panna",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "šlehačka"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/con-panna.png"
+        },
+        {
+          "id": 7,
+          "name": "Romano",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#fbdf5b",
+              "label": "citrón"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/romano.png"
+        },
+        {
+          "id": 8,
+          "name": "Cappuccino",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "mléčná pěna"
+            },
+            {
+              "color": "#fed7b0",
+              "label": "teplé mléko"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/cappuccino.png"
+        },
+        {
+          "id": 9,
+          "name": "Americano",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#b0dee1",
+              "label": "voda"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/americano.png"
+        },
+        {
+          "id": 10,
+          "name": "Café late",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "mléčná pěna"
+            },
+            {
+              "color": "#fed7b0",
+              "label": "teplé mléko"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/cafe-late.png"
+        },
+        {
+          "id": 11,
+          "name": "Flat white",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#fed7b0",
+              "label": "teplé mléko"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/flat-white.png"
+        },
+        {
+          "id": 12,
+          "name": "Marocchino",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "mléčná pěna"
+            },
+            {
+              "color": "#391d07",
+              "label": "čokoláda"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/marocchino.png"
+        },
+        {
+          "id": 13,
+          "name": "Mocha",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "mléčná pěna"
+            },
+            {
+              "color": "#fed7b0",
+              "label": "teplé mléko"
+            },
+            {
+              "color": "#391d07",
+              "label": "čokoláda"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/mocha.png"
+        },
+        {
+          "id": 14,
+          "name": "Bicerin",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "šlehačka"
+            },
+            {
+              "color": "#fed7b0",
+              "label": "teplé mléko"
+            },
+            {
+              "color": "#391d07",
+              "label": "čokoláda"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/bicerin.png"
+        },
+        {
+          "id": 15,
+          "name": "Breve",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "mléčná pěna"
+            },
+            {
+              "color": "#fed7b1",
+              "label": "mléko se smetanou"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/breve.png"
+        },
+        {
+          "id": 16,
+          "name": "Raf coffee",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#fdeeca",
+              "label": "smetana"
+            },
+            {
+              "color": "#ffb981",
+              "label": "vanilkový cukr"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/raf-coffee.png"
+        },
+        {
+          "id": 17,
+          "name": "Mead rad",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#fdeeca",
+              "label": "smetana"
+            },
+            {
+              "color": "#ffb603",
+              "label": "med"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/mead-raf.png"
+        },
+        {
+          "id": 18,
+          "name": "Vídeňská káva",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#feeeca",
+              "label": "šlehačka"
+            },
+            {
+              "color": "#613916",
+              "label": "espresso"
+            }
+          ],
+          "image": "./cups/vienna-coffee.png"
+        },
+        {
+          "id": 19,
+          "name": "Čokoláda s mlékem",
+          "ordered": false,
+          "layers": [
+            {
+              "color": "#fed7b0",
+              "label": "mléko"
+            },
+            {
+              "color": "#391d07",
+              "label": "čokoláda"
+            }
+          ],
+          "image": "./cups/chocolate-milk.png"
+        }
+      ]
+      export default drinks;
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,3 +1,7 @@
+import { Drink } from "../Drink"
+import React from "react"
+import {drinks} from '../../../public/drinks'
+
 export const Menu =()=>{
     return(<>
     <section id="menu" className="menu">
@@ -7,72 +11,9 @@ export const Menu =()=>{
             Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
           </p>
           <div className="drinks-list">
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/espresso.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Espresso</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }}></div>
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <form className="drink__controls">
-                <input type="hidden" className="order-id" value="0" />
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </form>
-            </div>
-
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/doppio.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Doppio</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <form className="drink__controls">
-                <input type="hidden" className="order-id" value="1" />
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </form>
-            </div>
-
-            <div className="drink">
-              <div className="drink__product">
-                <div className="drink__cup">
-                  <img src="/cups/lungo.png" />
-                </div>
-                <div className="drink__info">
-                  <h3>Lungo</h3>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#b0dee1' }} />
-                    <div className="layer__label">voda</div>
-                  </div>
-                  <div className="layer">
-                    <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-                    <div className="layer__label">espresso</div>
-                  </div>
-                </div>
-              </div>
-              <form className="drink__controls">
-                <input type="hidden" className="order-id" value="2" />
-                <button className="order-btn">
-                  Objednat
-                </button>
-              </form>
-            </div>
+          {drinks.map((prvek, index) => (
+              <Drink key={index} name={prvek.name} image={prvek.image} layers={prvek.layers}/>
+            ))}
           </div>
 
           <div className="order-detail">
